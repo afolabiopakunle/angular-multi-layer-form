50ff38cd439e53d97c62838cf29e83ff2b89d137
afolabiopakunle <opakunleafolabi@gmail.com>
2022-01-29 10:49:08 +0100

    main modules imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+
+  mainForm: FormGroup;
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 }
